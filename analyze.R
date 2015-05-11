4566a0d766a11602538775c350b2a99da174ea3c
Darshan-Josiah Barber <darshan@darshancomputing.com>
2015-05-11 13:21:15 -0700

    Include models with just IVs of interest.

--- analyze.R
+++ analyze.R
@@ -4,11 +4,24 @@ load('od.Rdata')
 
 options('scipen'=2)
 
-#summary(lm(od$crime ~ od$poverty + od$education + od$race + od$age + od$sex))
-#summary(lm(od$property ~ od$poverty + od$education + od$race + od$age + od$sex))
-#summary(lm(od$violent ~ od$poverty + od$education + od$race + od$age + od$sex))
+# Just DV and IV
+crime <- glm(od$crime ~ od$poverty, family = binomial(logit)) 
+poverty <- glm(od$property ~ od$poverty, family = binomial(logit))
+violent <- glm(od$violent ~ od$poverty, family = binomial(logit))
 
+summary(crime)
+summary(poverty)
+summary(violent)
 
+exp(coef(crime))
+exp(coef(poverty))
+exp(coef(violent))
+
+logLik(crime)
+logLik(poverty)
+logLik(violent)
+
+# DV, IV, and controls
 crime <- glm(od$crime ~ od$poverty + od$education + od$race + od$age + od$sex, family = binomial(logit)) 
 poverty <- glm(od$property ~ od$poverty + od$education + od$race + od$age + od$sex, family = binomial(logit))
 violent <- glm(od$violent ~ od$poverty + od$education + od$race + od$age + od$sex, family = binomial(logit))
